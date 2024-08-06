09d02efbcfd4d87302ce0784ff928b1920a7a5f1
“sosie-js” <sosie@sos-productions.com>
2024-08-06 22:33:44 +0200

    luarocks config files for linux declaring user and system trees

---
+++ config/config-5.1.lua
@@ -0,0 +1,17 @@
+--https://github.com/luarocks/luarocks/wiki/Config-file-format
+rocks_trees = {
+   {
+       name= "user",
+       root = home..[[/.luarocks]],
+       bin_dir = home.."/.local/bin"
+       --lib_dir = home.."/.local/lib/lua/5.1", 
+       --lua_dir = home.."/.local/share/lua/5.1
+   },
+   {  
+       name="system",
+       root = [[/usr/local]],
+       bin_dir = [[/usr/local/bin]]
+       --lib_dir = [[/usr/local/lua/5.1]], 
+       --lua_dir = [[/usr/local/share/lua/5.1]]
+   },
+}
