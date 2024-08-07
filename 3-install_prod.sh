25fd9f994ad057a06ec680366ee9f1a63a5c46f6
“sosie-js” <sosie@sos-productions.com>
2024-08-07 14:13:28 +0200

    Commented out

--- 3-install_prod.sh
+++ 3-install_prod.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #from https://martin-fieber.de/blog/create-build-publish-modules-for-lua/#publish-your-rock
-luarocks lint peagisub-1.0.0-5.rockspec
+luarocks lint peagisub-1.5.0-1.rockspec
 luarocks build --pack-binary-rock
-git tag v1.4.0 && git push --tags
+#tag is a snapshot, we do our best to match the branch name
+#if the branch evolved since this has to be moved to the new time to be up-to-date
+#see https://stackoverflow.com/questions/8044583/how-can-i-move-a-tag-on-a-git-branch-to-a-different-commit
+git tag v1.5.0 && git push --tags
 #to publish
-#./luarocks pack peagisub-1.0.0-5.rockspec
-#./luarocks upload peagisub-1.0.0-5.rockspec
+luarocks pack peagisub-1.5.0-1.rockspec
+luarocks upload peagisub-1.5.0-1.rockspec
