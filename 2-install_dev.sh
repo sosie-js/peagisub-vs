e7ae3e8c4084f196c98b827e8e3d0342eb18ba05
“sosie-js” <sosie@sos-productions.com>
2024-08-17 23:04:00 +0200

    Improved

--- 2-install_dev.sh
+++ 2-install_dev.sh
@@ -4,6 +4,8 @@ cp config/* ~/.luarocks/
 luarocks make --local
 #install the peagisub command not in .luarocks/bin but better..
 cp ~/.luarocks/bin/peagisub ~/.local/bin
+#this is mandatory to work, can be updated
+peagisub --createconfigfile
 echo "Peagisub installed , showing usage"
 peagisub -h
 
